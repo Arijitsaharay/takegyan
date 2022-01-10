235199701697548c5ca5d3ae723a1df08dd092e6
Arijit Saha Ray <52858374+Arijitsaharay@users.noreply.github.com>
2022-01-10 12:47:42 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "gyandan";
+$database = "gyandaan";
 
 $mysqli=$conn = mysqli_connect($servername,$username,$password,$database);
 
